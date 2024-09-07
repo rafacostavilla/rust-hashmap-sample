362d9a1d4869dcdbb4ecef49661dccc96488f5e2
rafacostavilla <rafaelcostavilla@gmail.com>
2024-09-07 15:34:25 +1200

    Use values overwriting, key and value adding only if key isn't present, and updating a value based on the old value techniques

--- src/main.rs
+++ src/main.rs
@@ -16,5 +16,46 @@ fn main (){
     for (key, value) in &scores {
         println!("The {key} Team score is: {value}");
     }
+    
+    println!("---------------------------------");
+    
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Red");
+    
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+    
+    //we can not use field_name and field_value anymore, because they moved into the map
+    // println!("{field_name}");
+    // println!("{field_value}");
+    println!("---------------------------------");
 
+    //Overwriting values
+    scores.insert(String::from("Blue"), 20);
+    scores.insert(String::from("Blue"), 35);
+
+    println!("{scores:?}");
+    
+    println!("---------------------------------");
+
+    //Adding a key and value only if key isn't present
+    scores.entry(String::from("Blue")).or_insert(105);
+    scores.entry(String::from("Purple")).or_insert(105);
+
+    println!("{scores:?}");
+    
+    println!("---------------------------------");
+
+    //Updating a value based on the old value
+    let text = "hello world wonderful world";
+
+    let mut hash_map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = hash_map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{hash_map:?}");
+    
 }
